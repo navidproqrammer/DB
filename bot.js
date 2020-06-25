84a52bc802d4d702eb51e95e9a10dcae8daa1ae5
Navidprogrammer <56113188+Navidprogrammer@users.noreply.github.com>
2020-06-25 23:44:24 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ bot.on('ready', async () =>{
 
 bot.on("message",message=>{
 
+  let args = message.content.substring(PREFIX.length).split(" ");
+
   if(message.author.bot) return
   if(message.content.startsWith(prefix +"dmall")){
   	
@@ -29,5 +31,25 @@ return message.channel.send("**You don't have permission to use that command**")
   }
 
 })
-
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong!')
+            break;
+       case 'clear':
+           if(!args[1]) return message.reply('Error please define second arg')
+           message.channel.bulkDelete(args[1]);
+           break;
+        case 'info':
+            if(args[1] === 'version'){
+                message.channel.send('Version ' + version);
+            }  else{
+                message.channel.send('Invalid Args');
+            }      
+            break;
+            case 'embed':
+                const embed = new Discord.richEmbed()
+                .addField('Name', message.author.username);
+                message.channel.send(embed);
+            break;    
+                  }
 bot.login(process.env.token);
